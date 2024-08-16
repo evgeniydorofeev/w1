7b9fe5f756033073158967392625bb5ea6a5bed6
evgeniydorofeev <evgeniy@moment.team>
2024-08-16 18:52:50 +0300

    update

--- src/main/java/w1/Controller1.java
+++ src/main/java/w1/Controller1.java
@@ -1,19 +1,28 @@
 package w1;
-import java.util.Map;
+import java.io.IOException;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import jakarta.servlet.http.HttpServletRequest; 
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse; 
 
 @RestController
 //@RequestMapping("") 
 public class Controller1 {
 
-	@GetMapping("/test") 
+	@GetMapping("/test")   
 	//@PreAuthorize("hasAuthority('ROLE_ADMIN')") 
-	public Map<String, String> user(HttpServletRequest rq) { 
-		return Map.of("xxx", "yyy"); 
+	public void test(HttpServletRequest rq, HttpServletResponse rs) throws IOException {
+		rs.getWriter().write("test");
+		//return Map.of("xxx", "yyy"); 
+	} 
+
+	@GetMapping("/admin")   
+	//@PreAuthorize("hasAuthority('ROLE_ADMIN')") 
+	public void admin(HttpServletRequest rq, HttpServletResponse rs) throws IOException {
+		rs.getWriter().write("admin");
+		//return Map.of("xxx", "yyy"); 
 	} 
 
 } 
